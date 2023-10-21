37d6cf868e425a92f2fd654f4c1ada369d283d4d
hugoalbert03 <hugoalbert.df@outlook.com>
2023-10-21 11:24:08 -0300

    0.1

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div class="graphic-bar">
+        <div class="colunas">
+            <div class="escala-y">
+                a
+            </div>
+            <div class="area-grafico">
+                <div class="area-barras">
+                    b
+                </div>
+                <div class="escala-x">
+                    c
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
